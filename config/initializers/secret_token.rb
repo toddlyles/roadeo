174af8c8e237d8014838f11fffcf11a9705d3f5e
Todd Lyles <toddlyles@gmail.com>
2012-05-29 18:27:10 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Roadeo::Application.config.secret_token = 'e71fb4680e50642827cd57602d77110b47e6ce1eb8351c3254e6cb4d5452bce0e38ca3c69bd3004266380aac5147e5b115f1de0965c9c0c68a184cb543ecf77f'
